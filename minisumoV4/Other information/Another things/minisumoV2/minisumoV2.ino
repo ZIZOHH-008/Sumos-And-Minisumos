ed64b900bdf9924b78195d14b9490cf3f767df23
Juan Parra <parrapradoj@gmail.com>
2026-06-10 10:47:57 -0500

    Add files via upload

---
+++ minisumoV4/Other information/Another things/minisumoV2/minisumoV2.ino
@@ -0,0 +1,231 @@
+// D0 y D1
+// D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12
+// A0, A1, A2, A3, A4, A5, A6, A7
+
+
+// ===== ULTRASONIDOS =====
+
+// IZQUIERDA
+const int triggerIzq = 8;
+const int echoIzq = 2;
+
+// CENTRO
+const int triggerCentro = 9;
+const int echoCentro = 10;
+
+// DERECHA
+const int triggerDer = A0;
+const int echoDer = A1;
+
+
+// ===== MOTOR A =====
+const int PWMA = 6; //D6
+const int AIN1 = 7; //D7
+const int AIN2 = 12; //D12
+
+// ===== MOTOR B =====
+const int PWMB = 3; //D3
+const int BIN1 = 5; //D5
+const int BIN2 = 4;//D4
+
+// ===== STANDBY =====
+const int STBY = 11; //D11
+
+
+void setup() {
+  pinMode(triggerIzq, OUTPUT);
+  pinMode(echoIzq, INPUT);
+
+  pinMode(triggerCentro, OUTPUT);
+  pinMode(echoCentro, INPUT);
+
+  pinMode(triggerDer, OUTPUT);
+  pinMode(echoDer, INPUT);
+
+  // MOTOR A
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+
+  // MOTOR B
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+
+  // STANDBY
+  pinMode(STBY, OUTPUT);
+
+  // Activar puente H
+  digitalWrite(STBY, HIGH);
+
+  detenerMotores();
+
+  Serial.begin(9600);
+
+  delay(4000);
+}
+
+
+
+//======== FUNCIONES DE DETECCIÓN =========
+float medirDistancia(int trigger, int echo) {
+
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigger, LOW);
+
+  long tiempo = pulseIn(echo, HIGH, 30000);
+
+  if (tiempo == 0) {
+    return 999;
+  }
+
+  return tiempo * 0.034 / 2.0;
+}
+
+
+void loop() {
+  float distanciaAtras = medirDistancia(triggerIzq, echoIzq);
+  delay(20);
+
+  float distanciaDerecha = medirDistancia(triggerCentro, echoCentro);
+  delay(20);
+
+  float distanciaAdelante = medirDistancia(triggerDer, echoDer);
+
+  const int DETECCION = 25;
+
+  bool rivalAtras = (distanciaAtras > 0) && (distanciaAtras <= DETECCION);
+  bool rivalDerecha = (distanciaDerecha > 0) && (distanciaDerecha <= DETECCION);
+  bool rivalAdelante = (distanciaAdelante > 0) && (distanciaAdelante <= DETECCION);
+
+
+  // ===== LÓGICA DEL MINISUMO =====
+  if (rivalAdelante) {
+    avanzar(130);  //DERECHA es AVANZAR
+  }
+
+  else if (rivalDerecha) {
+    girarDerecha(100); //Avanzar ES DERECHA
+  }
+
+  else if (rivalAtras) {
+    retroceder(100);   // iba para atras
+  } //girarizquieda es para atras
+  // para atras es izquieda
+
+  else {
+    girarIzquierda(150); 
+  }
+
+
+  // ===== Imprimir valores =====
+  Serial.print("Atr: ");
+  Serial.print(distanciaAtras);
+
+  Serial.print("  Der: ");
+  Serial.print(distanciaDerecha);
+
+  Serial.print("  Adel: ");
+  Serial.println(distanciaAdelante);
+
+  delay(50);
+}
+
+
+
+
+
+
+
+// ========= FUNCIONES DE MOVIMIENTO =========
+void activarDriver() {
+  digitalWrite(STBY, HIGH);
+}
+
+
+
+void desactivarDriver() {
+  digitalWrite(STBY, LOW);
+}
+
+
+
+void girarDerecha(int velocidad) {
+  activarDriver();
+
+  // Motor A
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA, velocidad);
+
+  // Motor B
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+  analogWrite(PWMB, velocidad);
+}
+
+
+
+void girarIzquierda(int velocidad) {
+  activarDriver();
+
+  // Motor A
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  analogWrite(PWMA, velocidad);
+
+  // Motor B
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);
+  analogWrite(PWMB, velocidad);
+}
+
+
+
+void retroceder(int velocidad) {
+  activarDriver();
+
+  // Izquierdo atrás
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  analogWrite(PWMA, velocidad);
+
+  // Derecho adelante
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+  analogWrite(PWMB, velocidad);
+}
+
+
+
+void avanzar(int velocidad) {
+  activarDriver();
+
+  // Izquierdo adelante
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA, velocidad);
+
+  // Derecho atrás
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);
+  analogWrite(PWMB, velocidad);
+}
+
+
+
+void detenerMotores() {
+  analogWrite(PWMA, 0);
+  analogWrite(PWMB, 0);
+
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
+
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, LOW);
+}
